118dd6e6b248d55131665f31a8a887bf0ce272d0
Xavier Lange <xrlange@gmail.com>
2015-11-19 18:06:57 -0800

    Hush up the traces

--- scamp/logger.go
+++ scamp/logger.go
@@ -10,13 +10,16 @@ var (
 	Error *log.Logger
 )
 
+type NullWriter int
+func (NullWriter) Write([]byte) (int, error) { return 0, nil }
+
 func initSCAMPLogger() {
 	// Idempotent logger setup!
 	if Trace != nil {
 		return;
 	}
 
-	Trace = log.New(os.Stdout, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Trace = log.New(new(NullWriter), "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Error = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
