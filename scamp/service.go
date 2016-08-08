7abd38b1ef81887e331b60211470592052f38224
senseijack <senseijack@gmail.com>
2016-08-08 16:24:13 -0700

    debug

--- scamp/service.go
+++ scamp/service.go
@@ -123,7 +123,7 @@ func (serv *Service)listen() (err error) {
   Trace.Printf("serv.listenerIP: `%s`", serv.listenerIP)
   Info.Printf("serv.listenerIP(before): %s", serv.listenerIP)
   serv.listenerIP = net.ParseIP("127.0.0.1"),
-  Info.Printf("serv.listenerIP(after): %s", serv.listenerIP)
+  // Info.Printf("serv.listenerIP(after): %s", serv.listenerIP)
   if err != nil {
   	return
   }
