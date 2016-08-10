b3fde8293a868190a378bf2c1b2f8591c3615f96
senseijack <senseijack@gmail.com>
2016-08-10 15:04:03 -0700

    debugging Marshal issue

--- scamp/service.go
+++ scamp/service.go
@@ -275,7 +275,7 @@ func (serv *Service)MarshalText() (b []byte, err error){
 		return
 	}
     Info.Printf("\nclassRecord JSON: %s\n", classRecord)
-    classRecord2 := `'[3,"sdk_service-KdhNHkxWGxURvzIxV+M1IuYF","channelmodule",1,2500,"beepish+tls://127.0.0.1:48386",["json"],[["SDK.orderpull”,[“order_fetch","",1]],["SDK.inventory",["invpush_transmit","",1]]],1470865574.128266]'`
+    classRecord2 := []byte([3,"sdk_service-KdhNHkxWGxURvzIxV+M1IuYF","channelmodule",1,2500,"beepish+tls://127.0.0.1:48386",["json"],[["SDK.orderpull”,[“order_fetch","",1]],["SDK.inventory",["invpush_transmit","",1]]],1470865574.128266])
 	sig, err := SignSHA256(classRecord2, serv.cert.PrivateKey.(*rsa.PrivateKey))
 	if err != nil {
 		return
